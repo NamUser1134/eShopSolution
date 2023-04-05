463fa6137526cd5314d0ba656dfc60e7cf6f0e41
MinhQuy <MinhQuy@gmail.com>
2023-04-06 01:26:26 +0700

    Add Migration DB

---
+++ eShopSolution.Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            // cau hinh config cho probility
+            builder.ToTable("AppConfigs"); // cấu hình cho bảng ten bảng có tên AppConfigs
+            builder.HasKey(x => x.Key);
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
